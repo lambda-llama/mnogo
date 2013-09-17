cd20c4bd23f8b0c705427c686b671af017085d1a
Fedor Gogolev <knsd@knsd.net>
2013-09-17 12:33:57 +0400

    Initial mongodbConnect implementation

--- src/Database/Mongodb/Connection.hs
+++ src/Database/Mongodb/Connection.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module Database.Mongodb.Connection
 	( MongodbHost, MongodbPort
 	, MongodbConnectionInfo(..)
@@ -7,9 +9,11 @@ module Database.Mongodb.Connection
 	) where
 
 import Data.Word (Word16)
+import qualified Data.ByteString.Char8 as StrictByteString
 
 import Control.Monad.Catch (MonadCatch, bracket)
 import Control.Monad.Trans (MonadIO(liftIO))
+import qualified Network.Socket as Socket
 
 import Database.Mongodb.Internal (StrictByteString)
 
@@ -19,10 +23,20 @@ type MongodbPort = Word16
 data MongodbConnectionInfo = MongodbConnectionInfoInet {-# UNPACK #-} !MongodbHost {-# UNPACK #-} !MongodbPort
 						   | MongodbConnectionInfoUnix {-# UNPACK #-} !StrictByteString
 
-data MongodbConnection = MongodbConnection
+data MongodbConnection = MongodbConnection Socket.Socket
 
 mongodbConnect :: MongodbConnectionInfo -> IO MongodbConnection
-mongodbConnect = error "mongodbConnect: not implemented"
+mongodbConnect (MongodbConnectionInfoInet host port) = do
+	(Socket.AddrInfo { .. }:_) <- Socket.getAddrInfo Nothing
+													 (Just $ StrictByteString.unpack host)
+													 (Just $ show port)
+	socket <- Socket.socket addrFamily addrSocketType addrProtocol
+	Socket.connect socket addrAddress
+	return $ MongodbConnection socket
+mongodbConnect (MongodbConnectionInfoUnix path) = do
+	socket <- Socket.socket Socket.AF_UNIX Socket.Stream Socket.defaultProtocol
+	Socket.connect socket $ Socket.SockAddrUnix $ StrictByteString.unpack path
+	return $ MongodbConnection socket
 
 mongodbClose :: MongodbConnection -> IO ()
 mongodbClose = error "mongodbClose: not implemented"
