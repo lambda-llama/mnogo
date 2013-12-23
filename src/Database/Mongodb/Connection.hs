b287cc7873f45daa7af466b22b4715d08df6e868
Fedor Gogolev <knsd@knsd.net>
2013-12-23 11:43:02 +0400

    Add counters to connection

--- src/Database/Mongodb/Connection.hs
+++ src/Database/Mongodb/Connection.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -funbox-strict-fields #-}
+
 {-# LANGUAGE RecordWildCards #-}
 
 module Database.Mongodb.Connection
@@ -14,31 +16,38 @@ import qualified Data.ByteString.Char8 as StrictByteString
 
 import qualified Network.Socket as Socket
 
-import Database.Mongodb.Internal (StrictByteString)
+import Database.Mongodb.Internal (StrictByteString, RequestIdCounter,
+                                  ObjectIdCounter, newRequestIdCounter,
+                                  newObjectIdCounter)
 
 type Host = StrictByteString
 type Port = Word16
 
-data ConnectionInfo = ConnectionInfoInet {-# UNPACK #-} !Host {-# UNPACK #-} !Port
-                    | ConnectionInfoUnix {-# UNPACK #-} !StrictByteString
+data ConnectionInfo = ConnectionInfoInet !Host !Port
+                    | ConnectionInfoUnix !StrictByteString
 
-data Connection = Connection Socket.Socket
+data Connection = Connection !RequestIdCounter !ObjectIdCounter !Socket.Socket
 
 connect :: ConnectionInfo -> IO Connection
-connect (ConnectionInfoInet host port) = do
-  (Socket.AddrInfo { .. }:_) <- Socket.getAddrInfo Nothing
-                           (Just $ StrictByteString.unpack host)
-                           (Just $ show port)
-  socket <- Socket.socket addrFamily addrSocketType addrProtocol
-  Socket.connect socket addrAddress
-  return $ Connection socket
-connect (ConnectionInfoUnix path) = do
-  socket <- Socket.socket Socket.AF_UNIX Socket.Stream Socket.defaultProtocol
-  Socket.connect socket $ Socket.SockAddrUnix $ StrictByteString.unpack path
-  return $ Connection socket
+connect info = do
+  socket <- case info of
+    (ConnectionInfoInet host port) -> do
+      (Socket.AddrInfo { .. }:_) <- Socket.getAddrInfo Nothing
+                               (Just $ StrictByteString.unpack host)
+                               (Just $ show port)
+      socket <- Socket.socket addrFamily addrSocketType addrProtocol
+      Socket.connect socket addrAddress
+      return socket
+    (ConnectionInfoUnix path) -> do
+      socket <- Socket.socket Socket.AF_UNIX Socket.Stream Socket.defaultProtocol
+      Socket.connect socket $ Socket.SockAddrUnix $ StrictByteString.unpack path
+      return socket
+  requestIdCounter <- newRequestIdCounter
+  objectIdCounter <- newObjectIdCounter
+  return $ Connection requestIdCounter objectIdCounter socket
 
 close :: Connection -> IO ()
-close (Connection socket) = Socket.close socket
+close (Connection _ _ socket) = Socket.close socket
 
 withConnection :: ConnectionInfo -> (Connection -> IO a) -> IO a
 withConnection info = bracket (connect info) close
--- src/Database/Mongodb/Internal.hs
+++ src/Database/Mongodb/Internal.hs
@@ -1,7 +1,30 @@
 module Database.Mongodb.Internal
-    ( StrictByteString
-	) where
+  ( StrictByteString
+  , RequestIdCounter(..)
+  , ObjectIdCounter(..)
+  , newRequestIdCounter
+  , newObjectIdCounter
+  ) where
 
+import Data.Int (Int32)
+import Data.IORef (IORef, newIORef)
+import System.Random (Random(..), randomIO)
 import qualified Data.ByteString.Char8 as StrictByteString
 
+import Data.Word.Word24 (Word24)
+
 type StrictByteString = StrictByteString.ByteString
+
+newtype RequestIdCounter = RequestIdCounter { unRequestIdCounter :: IORef Int32 }
+newtype ObjectIdCounter = ObjectIdCounter { unObjectIdCounter :: IORef Word24 }
+
+instance Random Word24 where
+  randomR (a, b) g = case randomR (fromEnum a, fromEnum b) g of
+    (x, g') -> (toEnum x, g')
+  random g = randomR (minBound, maxBound) g
+
+newRequestIdCounter :: IO RequestIdCounter
+newRequestIdCounter = fmap RequestIdCounter $ newIORef 0
+
+newObjectIdCounter :: IO ObjectIdCounter
+newObjectIdCounter = fmap ObjectIdCounter $ randomIO >>= newIORef
