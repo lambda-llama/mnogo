f345b182d29dea44cb9862090da439ed77e7d3d2
Sergei Lebedev <superbobry@gmail.com>
2013-12-25 22:57:43 +0400

    'Connection' is now a record

--- src/Database/Mongodb/Connection.hs
+++ src/Database/Mongodb/Connection.hs
@@ -1,5 +1,6 @@
 {-# OPTIONS_GHC -funbox-strict-fields #-}
 
+{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE RecordWildCards #-}
 
 module Database.Mongodb.Connection
@@ -16,38 +17,50 @@ import qualified Data.ByteString.Char8 as StrictByteString
 
 import qualified Network.Socket as Socket
 
-import Database.Mongodb.Internal (StrictByteString, RequestIdCounter,
-                                  ObjectIdCounter, newRequestIdCounter,
-                                  newObjectIdCounter)
+import Database.Mongodb.Internal (StrictByteString,
+                                  RequestIdCounter, newRequestIdCounter,
+                                  ObjectIdCounter, newObjectIdCounter)
 
 type Host = StrictByteString
-type Port = Word16
+type Port = Word16  -- FIXME(lebedev): why only 16 bits?
 
 data ConnectionInfo = ConnectionInfoInet !Host !Port
                     | ConnectionInfoUnix !StrictByteString
 
-data Connection = Connection !RequestIdCounter !ObjectIdCounter !Socket.Socket
+{- Note(lebedev): how about a phantom type, representing connection
+   state, i. e.
+
+     data Open
+     data Closed
+     data Connection phantom = ...
+
+   This is a classical phantom types example, but it's still useful one.
+  -}
+data Connection = Connection { conRidCounter :: !RequestIdCounter
+                             , conOidCounter :: !ObjectIdCounter
+                             , conSocket     :: !Socket.Socket
+                             }
 
 connect :: ConnectionInfo -> IO Connection
 connect info = do
-  socket <- case info of
-    (ConnectionInfoInet host port) -> do
+  conSocket <- case info of
+    ConnectionInfoInet host port -> do
       (Socket.AddrInfo { .. }:_) <- Socket.getAddrInfo Nothing
                                (Just $ StrictByteString.unpack host)
                                (Just $ show port)
       socket <- Socket.socket addrFamily addrSocketType addrProtocol
       Socket.connect socket addrAddress
       return socket
-    (ConnectionInfoUnix path) -> do
+    ConnectionInfoUnix path -> do
       socket <- Socket.socket Socket.AF_UNIX Socket.Stream Socket.defaultProtocol
       Socket.connect socket $ Socket.SockAddrUnix $ StrictByteString.unpack path
       return socket
-  requestIdCounter <- newRequestIdCounter
-  objectIdCounter <- newObjectIdCounter
-  return $ Connection requestIdCounter objectIdCounter socket
+  conRidCounter <- newRequestIdCounter
+  conOidCounter <- newObjectIdCounter
+  return Connection { .. }
 
 close :: Connection -> IO ()
-close (Connection _ _ socket) = Socket.close socket
+close (Connection { conSocket }) = Socket.close conSocket
 
 withConnection :: ConnectionInfo -> (Connection -> IO a) -> IO a
 withConnection info = bracket (connect info) close
